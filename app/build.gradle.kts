e3280c5cfd2d3444e0c413ff76f695cd545ad4aa
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-09-08 15:44:41 +0800

    Grades Panel - Complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("com.google.firebase:firebase-common:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -48,6 +49,11 @@ dependencies {
 
     implementation("com.google.firebase:firebase-auth:22.1.0")
     implementation("com.google.firebase:firebase-database:20.2.1")
+
+    implementation("com.mikhaellopez:circularimageview:4.3.1")
+    implementation("com.google.android.material:material:1.6.1")
+
+
 
 
 }
--- app/src/main/java/com/example/studentportal/fragment_Grades.java
+++ app/src/main/java/com/example/studentportal/fragment_Grades.java
@@ -7,58 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_Grades#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fragment_Grades extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String[] item = {"2023-2024(3rd Year - Second Sem)", "2024-2025(4th Year - First Sem)"};
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_Grades() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_Grades.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_Grades newInstance(String param1, String param2) {
-        fragment_Grades fragment = new fragment_Grades();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__grades, container, false);
+        View rootView = inflater.inflate(R.layout.fragment__grades, container, false);
+
+        autoCompleteTextView = rootView.findViewById(R.id.auto_complete_text);
+        adapterItems = new ArrayAdapter<>(getContext(), R.layout.list_item, item);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getContext(), "Item: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 }
