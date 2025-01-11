7ae88d81423f1db882568e7332b4c59c02a28148
Jayar <jrsaranza92@gmail.com>
2025-01-11 15:06:50 +0800

    EXAM SCHEDULE PUSH

--- app/src/main/java/com/example/studentportal/fragment_schedule.java
+++ app/src/main/java/com/example/studentportal/fragment_schedule.java
@@ -23,11 +23,16 @@ public class fragment_schedule extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
 
-        LinearLayout fishery, it, managementdep, teacher;
+        LinearLayout fishery, it, managementdep, teacher, fisheryexam, itexam, managementexam, teacherexam;
         fishery = rootView.findViewById(R.id.fisheries);
         it = rootView.findViewById(R.id.informationtech);
         managementdep = rootView.findViewById(R.id.management);
         teacher = rootView.findViewById(R.id.teachereducation);
+        fisheryexam = rootView.findViewById(R.id.fisheries1);
+        itexam = rootView.findViewById(R.id.informationtech1);
+        managementexam = rootView.findViewById(R.id.management1);
+        teacherexam = rootView.findViewById(R.id.teachereducation1);
+
 
 
         fishery.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +67,37 @@ public class fragment_schedule extends Fragment {
             }
         });
 
+        fisheryexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1fDRY7fOQcU1nTQrBjzYqvFkOgfU4jaeR"));
+                startActivity(browserIntent);
+            }
+        });
+
+        managementexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1QCzGOQd-EaVV7DQIzrvu2IDnp9Q45d8r"));
+                startActivity(browserIntent);
+            }
+        });
+
+        itexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1_5RBOik3Fsd-80AJDFoLFd5GnEKFqXjl"));
+                startActivity(browserIntent);
+            }
+        });
+
+        teacherexam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1LW2AMotQXTmzHiOTohduWlqX-8qs-YQx"));
+                startActivity(browserIntent);
+            }
+        });
         return rootView;
     }
 }
